8474a9f217ec02c72ef24b0288735f6d0560cc68
younglinam0222-rgb <younglinam0222@gmail.com>
2026-08-15 13:55:56 +0900

    Update lunar-javascript.d.ts

--- types/lunar-javascript.d.ts
+++ types/lunar-javascript.d.ts
@@ -1,12 +1,23 @@
 declare module 'lunar-javascript' {
-  class Lunar {
-    static fromYmd(year: number, month: number, day: number): Lunar
+  class LunarObj {
+    getYearInGanZhi(): string
+    getYearInGanZhiByLiChun(): string
+    getMonthInGanZhi(): string
+    getDayInGanZhi(): string
+    getTimeInGanZhi(): string
     getSolar(): Solar
   }
   class Solar {
+    static fromYmd(year: number, month: number, day: number): Solar
+    static fromYmdHms(year: number, month: number, day: number, hour: number, minute: number, second: number): Solar
     getYear(): number
     getMonth(): number
     getDay(): number
+    getLunar(): LunarObj
   }
-  export = Lunar
+  class Lunar {
+    static fromYmd(year: number, month: number, day: number): LunarObj
+  }
+  const exportObj: { Lunar: typeof Lunar; Solar: typeof Solar }
+  export = exportObj
 }
